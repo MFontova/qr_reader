275d40d20688e3f5a38768e2a5644b702e61ebdf
Marc Fontova <mfontovat@gmail.com>
2023-02-05 09:20:23 +0100

    175

---
+++ lib/providers/scan_list_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:qr_reader/models/scan_model.dart';
+import 'package:qr_reader/providers/db_provider.dart';
+
+class ScanListProvider extends ChangeNotifier {
+  List<ScanModel> scans = [];
+  String tipoSeleccionado = 'http';
+
+  nuevoScan(String valor) async {
+    final nuevoScan = ScanModel(valor: valor);
+    final id = await DBProvider.db.nuevoScan(nuevoScan);
+    nuevoScan.id = id;
+
+    if (tipoSeleccionado == nuevoScan.type) {
+      this.scans.add(nuevoScan);
+      notifyListeners();
+    }
+  }
+}

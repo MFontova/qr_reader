8843e5153c66f0f772f732fcf0ad8fa3a3cd8df0
Marc Fontova <mfontovat@gmail.com>
2023-02-07 14:45:39 +0100

    181

--- lib/screens/maps_history_screen.dart
+++ lib/screens/maps_history_screen.dart
@@ -1,38 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:qr_reader/providers/scan_list_provider.dart';
+import 'package:qr_reader/widgets/scan_tiles.dart';
 
 class MapsScreen extends StatelessWidget {
   const MapsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final scanListProvider = Provider.of<ScanListProvider>(context);
-
-    return ListView.builder(
-      itemCount: scanListProvider.scans.length,
-      itemBuilder: (context, index) => Dismissible(
-        key: UniqueKey(),
-        background: Container(
-          color: Colors.red,
-        ),
-        onDismissed: (DismissDirection direction) {
-          Provider.of<ScanListProvider>(context, listen: false)
-              .borrarScanPorId(scanListProvider.scans[index].id);
-        },
-        child: ListTile(
-          leading: Icon(
-            Icons.map,
-          ),
-          title: Text(scanListProvider.scans[index].valor),
-          subtitle: Text(scanListProvider.scans[index].id.toString()),
-          trailing: Icon(
-            Icons.keyboard_arrow_right,
-            color: Colors.grey,
-          ),
-          onTap: () => print(scanListProvider.scans[index].id),
-        ),
-      ),
-    );
+    return ScanTiles(tipo: 'geo');
   }
 }
